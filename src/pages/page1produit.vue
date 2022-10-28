5f516c12d9045aa5a2071772efe66264c257f301
nafissaarnone <nafissa.arnone@edu.univ-fcomte.fr>
2022-10-28 21:05:20 +0200

    renommer

--- src/pages/page1produit.vue
+++ src/pages/page1produit.vue
@@ -1,16 +1,17 @@
 <template>
 
 
-    <img class="" src="../../public/image/PHOTO MONTRES.jpg" alt="">
+    <img class="" src="../../public/image/montre1.jpg" alt="">
     <div class="bg-[#D9D9D9]">
         <div>
             <H2 class="underline text-center" font-fanwood-text to="/">
                 LE PRODUIT
             </H2>
         </div>
-       <div class="flex justify-center">
-        <img src="../../public/image/MONTRES-removebg-preview.png" alt="">
-        <img src="../../public/image/Group_7-removebg-preview.png" alt="">
+       // @ts-ignore
+        <div class="flex justify-center">
+        <img src="../../public/image/montre3.png" alt="">
+        <img src="../../public/image/montre2.png " alt=">
         </div>
 
     </div>
