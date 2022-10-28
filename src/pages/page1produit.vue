0c329fa3a77338dab48973129de1f926b068b0f8
nafissaarnone <nafissa.arnone@edu.univ-fcomte.fr>
2022-10-28 21:33:54 +0200

    déploiement

--- src/pages/page1produit.vue
+++ src/pages/page1produit.vue
@@ -2,17 +2,18 @@
 
 
     <img class="" src="../../public/image/montre1.jpg" alt="">
+   
     <div class="bg-[#D9D9D9]">
         <div>
             <H2 class="underline text-center" font-fanwood-text to="/">
                 LE PRODUIT
             </H2>
         </div>
-       // @ts-ignore
         <div class="flex justify-center">
         <img src="../../public/image/montre3.png" alt="">
-        <img src="../../public/image/montre2.png " alt=">
+        <img src="../../public/image/montre2.png " alt="">
+        </div>
         </div>
 
-    </div>
+ 
 </template>
